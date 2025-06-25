ef766e52327856e12c455c9262503603f9d94555
PLGVS <pedro.gaspar1214@gmail.com>
2025-06-25 14:48:45 -0300

    feat: Exception handling - findById
    Signed-off-by: PLGVS <pedro.gaspar1214@gmail.com>

--- src/main/java/com/PLGVS/course/services/UserService.java
+++ src/main/java/com/PLGVS/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.PLGVS.course.services;
 
 import com.PLGVS.course.entities.User;
 import com.PLGVS.course.repositories.UserRepository;
+import com.PLGVS.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
