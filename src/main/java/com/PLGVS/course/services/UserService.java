5fbf0db2f1df9a5ef43fa17e2bb5cec8b2755aab
PLGVS <pedro.gaspar1214@gmail.com>
2025-06-25 15:23:42 -0300

    feat: Exception handling - update
    Signed-off-by: PLGVS <pedro.gaspar1214@gmail.com>

--- src/main/java/com/PLGVS/course/services/UserService.java
+++ src/main/java/com/PLGVS/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.PLGVS.course.entities.User;
 import com.PLGVS.course.repositories.UserRepository;
 import com.PLGVS.course.services.exceptions.DatabaseException;
 import com.PLGVS.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -45,9 +46,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
